de6f74ab4d7e568d405df29be1a72120f028e910
Munna <shakibhassanmunna777@gmail.com>
2025-01-11 19:51:22 +0600

    up

---
+++ 2764.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int DD,MM,YY;
+    scanf("%d/%d/%d",&DD,&MM,&YY);
+    printf("%02d/%02d/%02d\n",MM,DD,YY);
+    printf("%02d/%02d/%02d\n",MM,DD,YY);
+    printf("%d-%d-%d\n",DD,MM,YY);
+}
