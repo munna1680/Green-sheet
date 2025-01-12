d9eca950e734caa8655a91d3130582d759c7aed9
Munna <shakibhassanmunna777@gmail.com>
2025-01-12 18:42:25 +0600

    up

--- 1930.c
+++ 1930.c
@@ -2,7 +2,7 @@
 int main(){
     int T1,T2,T3,T4;
     scanf("%d %d %d %d",&T1,&T2,&T3,&T4);
-    int max=((T1-1)+(T2-1)+(T3-1)+T4);
-    printf("%d\n",max);
+    int Max=((T1-1)+(T2-1)+(T3-1)+T4);
+    printf("%d\n",Max);
     return 0;
 }
