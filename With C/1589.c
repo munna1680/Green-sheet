0b9251058e882b6c0e336dc72c5bdbaf4dbced25
Munna <shakibhassanmunna777@gmail.com>
2025-05-09 23:46:45 +0600

    java

--- With C/1589.c
+++ With C/1589.c
@@ -3,8 +3,11 @@ int main()
 {
     int n;
     scanf("%d",&n);
+    
     int a,b;
+
     int arr[n];
+
     for(int i = 0; i < n; i++)
     {
         scanf("%d %d",&a,&b);
--- With Java/Solve with Java/src/Bob_Conduit.java
+++
@@ -1,6 +0,0 @@
-//B-1589
-public class Bob_Conduit {
-    public static void main(String[] args) {
-        
-    }
-}
