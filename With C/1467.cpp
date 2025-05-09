4db1a45b81da7874a52e5679f3ac81d6dbf7b781
Munna <shakibhassanmunna777@gmail.com>
2025-05-10 00:36:18 +0600

    ad hoc

--- With C/1467.cpp
+++ With C/1467.cpp
@@ -1,16 +1,31 @@
 #include<iostream>
+
 using namespace std;
 
 int main()
 {
     int a,b,c;
+
     while(cin >> a >> b >> c)
     {
-        if(a == b && a != c) cout << "C" << endl;
-        else if(b == c && a != b && a != c) cout << "A" << endl;
-        else if(a == c && a != b && b != c) cout << "B" << endl;
-        else  cout << "*" << endl;
-   
+        if(a == b && a != c) 
+        {
+            cout << "C" << endl;
+        }
+        else if(b == c && a != b && a != c) 
+        {
+            cout << "A" << endl;
+        }
+        else if(a == c && a != b && b != c) 
+        {
+            cout << "B" << endl;
+        }
+        else 
+        {
+            cout << "*" << endl;
+        }
+        
+        return 0;
     }
     
 
