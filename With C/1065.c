6e0f63ecce7e695c1eedd8a983d51323aa4640cf
Munna <shakibhassanmunna777@gmail.com>
2025-05-10 23:26:44 +0600

    c

--- With C/1065.c
+++ With C/1065.c
@@ -3,12 +3,18 @@
 int main()
 {
     int i,num,count=0;
-    for(i=1;i<=5;i++){
+
+    for(i=1;i<=5;i++)
+    {
         scanf("%d",&num);
-        if(num%2==0){
+
+        if(num%2==0)
+        {
           count++;
         }
     }
+    
     printf("%d valores pares\n",count);
+    
     return 0;
 }
